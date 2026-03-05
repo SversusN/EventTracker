1c6b8f5d2c6cfaff8740d31a958dee2970f01d86
SversusN <skaldnord@yandex.ru>
2026-03-05 23:01:38 +0300

    program.cs + swagger

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 using EventTracker.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IEventService, EventService>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
