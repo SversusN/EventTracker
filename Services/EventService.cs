ada335dd5be26428a08f4efcb0b366743c411d0a
SversusN <skaldnord@yandex.ru>
2026-03-05 22:38:08 +0300

    thread safety

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -7,17 +7,24 @@ namespace EventTracker.Services;
 public class EventService : IEventService
 {
     private readonly List<Event> _events = [];
-
+    //List не потокобезопасен, можно 
+    private readonly Lock _lock = new();
 
     public IEnumerable<EventResponseDto> GetAllEvents()
     {
-         return _events.Select(MapToResponseDto).ToList();
+        lock (_lock)
+        {
+            return _events.Select(MapToResponseDto).ToList();
+        }
     }
 
     public EventResponseDto? GetEventById(Guid id)
     {
+        lock (_lock)
+        {
             var ev = _events.FirstOrDefault(e => e.Id == id);
             return ev is null ? null : MapToResponseDto(ev);
+        }
     }
 
     public EventResponseDto CreateEvent(CreateEventDto dto)
@@ -29,12 +36,18 @@ public class EventService : IEventService
             dto.EndAt
         );
 
+        lock (_lock)
+        {
             _events.Add(ev);
+        }
+
         return MapToResponseDto(ev);
     }
 
     public EventResponseDto? UpdateEvent(Guid id, UpdateEventDto dto)
     {
+        lock (_lock)
+        {
             var ev = _events.FirstOrDefault(e => e.Id == id);
             if (ev is null)
             {
@@ -49,21 +62,24 @@ public class EventService : IEventService
             );
 
             return MapToResponseDto(ev);
-     }
-    
+        }
+    }
 
     public bool DeleteEvent(Guid id)
     {
+        lock (_lock)
+        {
             var ev = _events.FirstOrDefault(e => e.Id == id);
             if (ev is null)
             {
                 return false;
             }
+
             _events.Remove(ev);
             return true;
         }
-   
-    //маппиннг в ответ
+    }
+
     private static EventResponseDto MapToResponseDto(Event ev)
     {
         return new EventResponseDto(
@@ -75,4 +91,3 @@ public class EventService : IEventService
         );
     }
 }
-
