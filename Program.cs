34aadd3b34c5cc7c0735d50b721080ce640fceff
SversusN <skaldnord@yandex.ru>
2026-05-13 22:14:00 +0300

    pfff

--- Program.cs
+++ Program.cs
@@ -38,6 +38,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    //не забыть дропнуть бд
     db.Database.Migrate();
 }
 
