328468b4617011dca98b21174f53a9326d55bbcd
SversusN <skaldnord@yandex.ru>
2026-05-13 21:18:44 +0300

    repos

---
+++ DataAccess/Repositories/BookingRepository.cs
@@ -0,0 +1,36 @@
+using EventTrackerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventTrackerApi.DataAccess.Repositories;
+
+public class BookingRepository(AppDbContext context) : IBookingRepository
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<Booking?> GetByIdAsync(Guid id)
+    {
+        return await _context.Bookings.FirstOrDefaultAsync(b => b.Id == id);
+    }
+
+    public async Task<IEnumerable<Booking>> GetPendingAsync()
+    {
+        return await _context.Bookings
+            .Where(b => b.Status == BookingStatus.Pending)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Booking booking)
+    {
+        await _context.Bookings.AddAsync(booking);
+    }
+
+    public void Update(Booking booking)
+    {
+        _context.Bookings.Update(booking);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
